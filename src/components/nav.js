9935e0ec982ee29dcb45837c822d130e16200b20
AlanDuong <d.alan@tx-intl.com>
2021-07-25 17:35:23 -0500

    halfway done with nav added picture of myself, name, degree and social links styled everything regarding to that

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import AlanProfilePic from '../images/AlanProfilePic.jpg';
+import GithubIcon from '../images/GitHub-white-logo-sm.png';
+import LinkedInIcon from '../images/linked-in-icon-white.png';
 function nav() {
     return (
         <div id="nav">
-            
+            <div id="nav-wrapper">
+                <img src={AlanProfilePic} alt="Alan Profile Pic"/>
+                <p>Alan Duong</p>
+                <a href="https://cs.rice.edu/" target="_blank">CS at Rice University </a>
+                <div id="social-links-container">
+                    <a href="https://www.linkedin.com/in/alan-duong-547095199/" target="_blank">
+                        <img src={LinkedInIcon} alt="Link to my Linked In Page"/>
+                    </a>
+                    <a href="https://github.com/AlanDuong07" target="_blank">
+                        <img src={GithubIcon} alt="Link to my Github" id="github-icon"/>
+                    </a>
+                    
+                </div>
+            </div>
         </div>
     )
 }
