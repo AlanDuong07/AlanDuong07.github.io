9dc930fc5f84e2def7bc60f7b4d7140297e73d61
AlanDuong <d.alan@tx-intl.com>
2021-07-25 15:47:36 -0500

    Finished intro content section and styling

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import UndrawCoding from '../undraw_coding.svg';
 function intro() {
     return (
         <div id="intro" className="grid-item">
-            
+            <div id="intro-wrapper">
+                <div id="intro-content">
+                    <h3>Alan Duong</h3>
+                    <h5>Software Engineer</h5>
+                    <button type="button" className="button">
+                        Let's have a talk!
+                    </button>
+                </div>
+                <div id="intro-image">
+                    <img src={UndrawCoding} alt="Alan Coding at a Laptop"/> 
+                </div>
+            </div>
         </div>
     )
 }
